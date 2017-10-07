04e65a57b33b6118d10fd2428ca7db48419eff56
viedia <laetitia.lemiere@gmail.com>
2017-10-07 06:03:46 +0200

    ajout de la telekinesie

---
+++ Assets/Telekinesie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telekinesie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Caisse")
+        {
+            other.gameObject.transform.position = new Vector3(other.gameObject.transform.position.x, other.gameObject.transform.position.y + 5, other.gameObject.transform.position.z);
+        }
+    }
+}
