6f20fb6261b2fbafea644adb1a48e5742bd59adc
viedia <laetitia.lemiere@gmail.com>
2017-10-07 22:30:29 +0200

    Ajout Vie et mort + chgt de gestion de deplacement de platform

---
+++ Assets/VieMort.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VieMort : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Caisse")
+        {
+            Debug.Log("Caisse");
+            FindObjectOfType<SwitchScene>().mourir();
+            Destroy(this.gameObject);
+        }
+        else if (collision.gameObject.tag == "Sortie")
+        {
+            Debug.Log("Sortie");
+            FindObjectOfType<SwitchScene>().sauver();
+        }
+    }
+}
