bb3232ec6a5b2c3942e75918713f66e532caa7d3
Aedrinios <clementclerc2@gmail.com>
2017-10-07 17:17:35 -0400

    Add cards display and bugs correction in Linedictionary

---
+++ Assets/CardHover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardHover : MonoBehaviour {
+	public Sprite basicSprite;
+	public Sprite hoverSprite;
+	SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseOver(){
+		sr.sprite = hoverSprite;
+	}
+
+	void OnMouseExit(){
+		sr.sprite = basicSprite;
+	}
+}
