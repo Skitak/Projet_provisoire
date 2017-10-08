9623724ac63039d6558fadfeb20a526cabe01305
skitak <bas2205@live.fr>
2017-10-08 15:25:13 -0400

    initial scene added

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	public float tempsPassage = 3f;
+	public string nextScene;
+
+	void Update () {
+		tempsPassage -= Time.deltaTime;
+		if (tempsPassage < 0)
+			SceneManager.LoadScene (nextScene);
+	}
+}
