a6dbb3e58eec2911a7ee0efc42898a96e04536ef
charles33u <anne-laure.charles1@uqac.ca>
2017-10-08 06:39:43 +0200

    j'espère que ça marche

---
+++ Assets/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    float timeLeft = 200.0f;
+    // Update is called once per frame
+    void Update () {
+        text.text = "time Left:" + timeLeft;
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            SceneManager.LoadScene("Level_3");
+        }
+
+    }
+}
